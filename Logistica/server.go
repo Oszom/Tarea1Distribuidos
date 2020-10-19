24bd1b53e8e91ccd62ebf7471e1eb934efd32f99
Jorge Galleguillos <jorge.galleguillo.14@sansano.usm.cl>
2020-10-19 16:19:41 -0300

    Comienzo de finanzas

--- Logistica/server.go
+++ Logistica/server.go
@@ -30,6 +30,21 @@ func main() {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 
+	lis2,err2 := net.Listen("tcp", ":9100")
+	if err2 != nil {
+		log.Fatalf("Failed to listen on port 9100: %v", err)
+	}
+
+	lis3,err3 := net.Listen("tcp", ":9101")
+	if err3 != nil {
+		log.Fatalf("Failed to listen on port 9101: %v", err)
+	}
+
+	lis4,err4 := net.Listen("tcp", ":9102")
+	if err4 != nil {
+		log.Fatalf("Failed to listen on port 9102: %v", err)
+	}
+
 	s := logistica.ServerLogistica{}
 
 	grpcServer := grpc.NewServer()
@@ -38,6 +53,18 @@ func main() {
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+
+	if err := grpcServer.Serve(lis2); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9100: %v", err)
+	}
+
+	if err := grpcServer.Serve(lis3); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9101: %v", err)
+	}
+
+	if err := grpcServer.Serve(lis4); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9102: %v", err)
 	}
 
 }
