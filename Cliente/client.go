7fad48d509e2f92439434ed3dcea4c1f3f14fb9f
Pablo <pablo.villalonc.14@sansano.usm.cl>
2020-10-19 21:25:29 -0300

    Lógica de ingresar pedidos/codigo de seguimiento y tipo de cliente

--- Cliente/client.go
+++ Cliente/client.go
@@ -3,13 +3,15 @@ package main
 import (
 	logistica "Tarea1/Logistica/logistica"
 	"bufio"
+	//"encoding/csv"
 	"fmt"
-	"log"
-	"os"
-	"strings"
-
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	//"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -19,6 +21,18 @@ func main() {
 	ip, _ := reader.ReadString('\n')
 	ip = strings.TrimSuffix(ip, "\n")
 	ip = strings.TrimSuffix(ip, "\r")
+
+	readerTipo := bufio.NewReader(os.Stdin)
+	fmt.Printf("Ingrese Tipo de cliente (pyme/retail): ")
+	tipo, _ := readerTipo.ReadString('\n')
+	tipo = strings.TrimSuffix(tipo, "\n")
+	tipo = strings.TrimSuffix(tipo, "\r")
+
+	readerTiempo := bufio.NewReader(os.Stdin)
+	fmt.Printf("Ingrese tiempo de espera entre ordenes (en segundos): ")
+	tiempo, _ := readerTiempo.ReadString('\n')
+	tiempo = strings.TrimSuffix(tiempo, "\n")
+	tiempo = strings.TrimSuffix(tiempo, "\r")
 
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial(ip+":9000", grpc.WithInsecure())
@@ -30,28 +44,100 @@ func main() {
 
 	c := logistica.NewLogisticaServiceClient(conn)
 
-	message1 := logistica.OrdenCliente{
-		Id:          "ASS-1313",
-		Producto:    "Un masajeador wink wink",
-		Valor:       1313,
-		Tienda:      "Solo Para Chicos Grandes",
-		Destino:     "Tus Nalgas",
-		Prioritario: -1,
+	var message1 logistica.OrdenCliente
+	var message2 logistica.SeguimientoCliente
+	for {
+
+		if tipo == "pyme" {
+			readerDecision := bufio.NewReader(os.Stdin)
+			fmt.Printf("Indique que quiere realizar (Pedido/Seguimiento)")
+			decision, _ := readerDecision.ReadString('\n')
+			decision = strings.TrimSuffix(decision, "\n")
+			decision = strings.TrimSuffix(decision, "\r")
+
+			if decision == "Pedido" {
+				readerPedido := bufio.NewReader(os.Stdin)
+				fmt.Printf("Ingrese su pedido con el siguiente formato: id,producto,valor,tienda,destino,prioritario: ")
+				pedido, _ := readerPedido.ReadString('\n')
+				pedido = strings.TrimSuffix(pedido, "\n")
+				pedido = strings.TrimSuffix(pedido, "\r")
+				pedidoListo := strings.Split(pedido, ",")
+
+				valorEntero, cosita := strconv.ParseInt(pedidoListo[2], 10, 64)
+				if cosita != nil {
+					log.Fatalf("Error al covertir el valor a entero: %s", err)
+				}
+
+				prioritarioEntero, cosito := strconv.ParseInt(pedidoListo[5], 10, 64)
+				if cosito != nil {
+					log.Fatalf("Error al covertir prioritario a entero: %s", err)
+				}
+				message1 = logistica.OrdenCliente{
+					Id:          pedidoListo[0],
+					Producto:    pedidoListo[1],
+					Valor:       valorEntero,
+					Tienda:      pedidoListo[3],
+					Destino:     pedidoListo[4],
+					Prioritario: prioritarioEntero,
+				}
+				response1, err1 := c.NuevaOrden(context.Background(), &message1)
+				if err1 != nil {
+					log.Fatalf("Problema al procesar su orden: %s", err)
+				}
+				log.Printf("El numero de seguimiento del producto %s es: %d", response1.Producto, response1.Seguimiento)
+
+			} else {
+				readerSeguimiento := bufio.NewReader(os.Stdin)
+				fmt.Printf("Ingrese codigo seguimiento")
+				seguimiento, _ := readerSeguimiento.ReadString('\n')
+				seguimiento = strings.TrimSuffix(seguimiento, "\n")
+				seguimiento = strings.TrimSuffix(seguimiento, "\r")
+				seguimientoEntero, cosite := strconv.ParseInt(seguimiento, 10, 64)
+				if cosite != nil {
+					log.Fatalf("Error al covertir seguimiento a entero: %s", err)
+				}
+
+				message2 = logistica.SeguimientoCliente{
+					Seguimiento: seguimientoEntero,
+					Estado:      "---",
+					Producto:    "---",
+				}
+
+				response2, err2 := c.InformarSeguimiento(context.Background(), &message2)
+				if err2 != nil {
+					log.Fatalf("Problema al consultar por su producto: %s", err)
+				}
+				log.Printf("Tu pedido se encuentra en %s", response2.Estado)
+
+			}
+
+		} else {
+			readerPedido := bufio.NewReader(os.Stdin)
+			fmt.Printf("Ingrese su pedido con el siguiente formato: id,producto,valor,tienda,destino: ")
+			pedido, _ := readerPedido.ReadString('\n')
+			pedido = strings.TrimSuffix(pedido, "\n")
+			pedido = strings.TrimSuffix(pedido, "\r")
+			pedidoListo := strings.Split(pedido, ",")
+
+			valorEntero, cosita := strconv.ParseInt(pedidoListo[2], 10, 64)
+			if cosita != nil {
+				log.Fatalf("Error al covertir el valor a entero: %s", err)
+			}
+			message1 = logistica.OrdenCliente{
+				Id:          pedidoListo[0],
+				Producto:    pedidoListo[1],
+				Valor:       valorEntero,
+				Tienda:      pedidoListo[3],
+				Destino:     pedidoListo[4],
+				Prioritario: -1,
+			}
+			response1, err1 := c.NuevaOrden(context.Background(), &message1)
+			if err1 != nil {
+				log.Fatalf("Problema al procesar su orden: %s", err)
+			}
+			log.Printf("El numero de seguimiento del producto %s es: %d", response1.Producto, response1.Seguimiento)
+		}
+
 	}
 
-	message2 := logistica.SeguimientoCliente{
-		Seguimiento: 1,
-		Estado:      "Un masajeador wink wink",
-		Producto:    "1313",
-	}
-
-	response1, err := c.NuevaOrden(context.Background(), &message1)
-	response2, err := c.InformarSeguimiento(context.Background(), &message2)
-
-	if err != nil {
-		log.Fatalf("La polilla gigante ataco la conexion: %s", err)
-	}
-
-	log.Printf("El numero de seguimiento de la wea de producto %s es: %d", response1.Producto, response1.Seguimiento)
-	log.Printf("El pedido ql que querí saber esta %s", response2.Estado)
 }
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+Integrantes:
+    Pablo Villalón C. 201473584-7
+    Jorge Galleguillos M. 201473545-6
+
+Recomendaciones:
+    - Al momento de ingresar el tipo de cliente porfavor respetar el formato mostrado en pantalla, respetando
+    mayúsculas y minúscula.
+    - Al momento de ingresar una orden porfavor respetar el formato mostrado en pantalla cada campo del pedido 
+    separado por "," y sin espacios entre medio.
+    
